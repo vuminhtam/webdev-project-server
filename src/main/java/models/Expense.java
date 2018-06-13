abfbd7b6af6c7b9e424516c3e7d34bda99ffb20f
Tam Vu <vu.t@husky.neu.edu>
2018-06-12 23:20:02 -0400

    expense entity

--- src/main/java/models/Expense.java
+++ src/main/java/models/Expense.java
@@ -1,5 +1,42 @@
 package models;
 
-public class Expense {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Expense {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private int ammount;
+	private Group group;
+	private User expenser; 
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAmmount() {
+		return ammount;
+	}
+	public void setAmmount(int ammount) {
+		this.ammount = ammount;
+	}
+	public Group getGroup() {
+		return group;
+	}
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+	public User getExpenser() {
+		return expenser;
+	}
+	public void setExpenser(User expenser) {
+		this.expenser = expenser;
+	}
+	
 }
