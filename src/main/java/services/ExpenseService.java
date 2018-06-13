0ef2b0d95702b5d9e7a887fddeaf4e9337922aa3
Tam Vu <vu.t@husky.neu.edu>
2018-06-13 01:08:50 -0400

    singleTable

--- src/main/java/services/ExpenseService.java
+++ src/main/java/services/ExpenseService.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,10 @@ import com.example.webdevsummer12018.models.EssayQuestion;
 import com.example.webdevsummer12018.models.Exam;
 import com.example.webdevsummer12018.models.Group;
 import com.example.webdevsummer12018.models.Question;
+import com.example.webdevsummer12018.models.User;
 
 import models.Expense;
+import models.TravelExpense;
 import repositories.AccomodationExpenseRepository;
 import repositories.ExpenseRepository;
 import repositories.FoodExpenseRepository;
@@ -29,17 +32,19 @@ import repositories.TravelExpenseRepository;
 public class ExpenseService {
 	@Autowired 
 	GroupRepository groupRepo;
+	@Autowired 
+	UserRepository userRepo;
 	
 	@Autowired 
 	ExpenseRepository expenseRepo;
-	@Autowired 
-	TravelExpenseRepository travelExpRepo;
-	@Autowired 
-	FoodExpenseRepository foodExpRepo;
-	@Autowired 
-	AccomodationExpenseRepository accomodationExpRepo;
-	@Autowired 
-	OtherExpenseRepository otherExpRepo;
+	@Autowired  
+	  TravelExpenseRepository travelExpRepo; 
+	  @Autowired  
+	  FoodExpenseRepository foodExpRepo; 
+	  @Autowired  
+	  AccomodationExpenseRepository accomodationExpRepo; 
+	  @Autowired  
+	  OtherExpenseRepository otherExpRepo; 
 	
 	@GetMapping("/api/group/{groupId}/expense")
 	public List<Expense> getAllExpenseByGroup(@PathVariable("groupId") int groupId) {
@@ -54,48 +59,61 @@ public class ExpenseService {
 		}
 	}
 	
-	@DeleteMapping("/api/{expenseType}/{id}")
-	public void deleteQuestionByID(@PathVariable("id") int id) {
+	@DeleteMapping("/api/expense/{id}")
+	public void deleteExpenseByID(@PathVariable("id") int id) {
 		expenseRepo.deleteById(id);
 	}
 	
-//	@GetMapping("/api/{expenseType}/{eid}")
-//	public Expense getExpenseByIdAndType(
-//			@PathVariable("expenseType") String expenseType,
-//			@PathVariable("eid") int eid) {
-//		try {
-//			switch(expenseType) {
-//			case "travel": return travelExpRepo.findById(eid).get(); break;
-//			case "accomodation": return accomodationExpRepo.findById(eid).get(); break;
-//			case "food": return foodExpRepo.findById(eid).get(); break;
-//			case "other": return otherExpRepo.findById(eid).get(); break;
-//			default: throw new IllegalArgumentException("No such expense of type " + expenseType);
-//			}
-//		} 
-//		catch (Exception e) {
-//			throw new IllegalArgumentException("Cannot find expense " + eid + " of type " + expenseType);
-//		}
-//	}
+	@GetMapping("/api/expense/{id}")
+	public Expense getExpenseById(@PathVariable("id") int id) {
+		Optional<Expense> optional = expenseRepo.findById(id);
+		if(optional.isPresent()) {
+			return optional.get();
+		}
+		else {
+			throw new IllegalArgumentException("Cannot find expense " + id);
+		}
+	}
 	
-//	@PostMapping("/api/group/{groupId}/{expenseType}")
-//	public void addExpenseByTypeToGroup(
-//			@PathVariable("expenseType") String expenseType,
-//			@PathVariable("groupId") int groupId) {
-//		Optional<Group> res = groupRepo.findById(groupId);
-//		if(res.isPresent()) {
-//			Group group = res.get();
-//			
-//		}
-//		else {
-//			throw new IllegalArgumentException("Cannot find topic " + id);
-//		}
-//	}
-	
+	@PostMapping("/api/user/{uid}/group/{groupId}/expense")
+	public Expense addExpenseToGroup(
+			@PathVariable("uid") int uid,
+			@PathVariable("groupId") int groupId,
+			@RequestBody Expense newExpense) {
+		if(newExpense.getType() == null || newExpense.getType().equals("")) {
+			throw new IllegalArgumentException("Null type");
+		}
+		Optional<User> user = userRepo.findById(uid);
+		Optional<Group> group = groupRepo.findById(groupId);
+		if(group.isPresent() && user.isPresent()) {
+			newExpense.setGroup(group);
+			newExpense.setExpenser(user);
+			return expenseRepo.save(newExpense);
+		}
+		else {
+			throw new IllegalArgumentException("Cannot find group " + groupId + " or user " + uid);
+		}
+	}
 	
 	
-	
-	
-	
-	
+	@PutMapping("/api/travel/{id}")
+	public TravelExpense updateTravelExpenseById(
+			@PathVariable("id") int id,
+			@RequestBody TravelExpense newExpense) {
+		Optional<TravelExpense> retrieve = travelExpRepo.findById(id);
+		if(retrieve.isPresent()) {
+			TravelExpense expense = retrieve.get();
+			expense.setAmmount(newExpense.getAmmount());
+			expense.setExpenser(newExpense.getExpenser());
+			expense.setFrom(newExpense.getFrom());
+			expense.setTo(newExpense.getTo());
+			expense.setNote(newExpense.getNote());
+			return expense;
+		}
+		else {
+			throw new IllegalArgumentException("Cannot find expense " + id);
+		}
+	}
+
 	
 }
