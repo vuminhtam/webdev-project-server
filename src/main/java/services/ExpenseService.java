62736481fffc5e3a602aee929aef628814de750a
Tam Vu <vu.t@husky.neu.edu>
2018-06-13 00:37:46 -0400

    before change to single table

--- src/main/java/services/ExpenseService.java
+++ src/main/java/services/ExpenseService.java
@@ -5,12 +5,16 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webdevsummer12018.models.EssayQuestion;
 import com.example.webdevsummer12018.models.Exam;
+import com.example.webdevsummer12018.models.Group;
 import com.example.webdevsummer12018.models.Question;
 
 import models.Expense;
@@ -50,21 +54,48 @@ public class ExpenseService {
 		}
 	}
 	
-	@GetMapping("/api/{expenseType}/{eid}")
-	public Expense getExpenseById(
-			@PathVariable("expenseType") String expenseType,
-			@PathVariable("eid") int eid) {
-		try {
-			switch(expenseType) {
-			case "travel": return travelExpRepo.findById(eid).get(); break;
-			case "accomodation": return accomodationExpRepo.findById(eid).get(); break;
-			case "food": return foodExpRepo.findById(eid).get(); break;
-			case "other": return otherExpRepo.findById(eid).get(); break;
-			default: throw new IllegalArgumentException("No such expense of type " + expenseType);
-			}
-		} 
-		catch (Exception e) {
-			throw new IllegalArgumentException("Cannot find expense " + eid + " of type " + expenseType);
-		}
+	@DeleteMapping("/api/{expenseType}/{id}")
+	public void deleteQuestionByID(@PathVariable("id") int id) {
+		expenseRepo.deleteById(id);
 	}
+	
+//	@GetMapping("/api/{expenseType}/{eid}")
+//	public Expense getExpenseByIdAndType(
+//			@PathVariable("expenseType") String expenseType,
+//			@PathVariable("eid") int eid) {
+//		try {
+//			switch(expenseType) {
+//			case "travel": return travelExpRepo.findById(eid).get(); break;
+//			case "accomodation": return accomodationExpRepo.findById(eid).get(); break;
+//			case "food": return foodExpRepo.findById(eid).get(); break;
+//			case "other": return otherExpRepo.findById(eid).get(); break;
+//			default: throw new IllegalArgumentException("No such expense of type " + expenseType);
+//			}
+//		} 
+//		catch (Exception e) {
+//			throw new IllegalArgumentException("Cannot find expense " + eid + " of type " + expenseType);
+//		}
+//	}
+	
+//	@PostMapping("/api/group/{groupId}/{expenseType}")
+//	public void addExpenseByTypeToGroup(
+//			@PathVariable("expenseType") String expenseType,
+//			@PathVariable("groupId") int groupId) {
+//		Optional<Group> res = groupRepo.findById(groupId);
+//		if(res.isPresent()) {
+//			Group group = res.get();
+//			
+//		}
+//		else {
+//			throw new IllegalArgumentException("Cannot find topic " + id);
+//		}
+//	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
