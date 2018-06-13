f08b194d005bcf797c45dfd18ddf2c7320115656
Tam Vu <vu.t@husky.neu.edu>
2018-06-13 00:23:46 -0400

    getExpenseByIdAndType

--- src/main/java/models/FoodExpense.java
+++ src/main/java/models/FoodExpense.java
@@ -1,5 +1,17 @@
 package models;
 
-public class FoodExpense {
+import javax.persistence.Entity;
 
+@Entity
+public class FoodExpense extends Expense {
+	private String restaurant;
+
+	public String getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(String restaurant) {
+		this.restaurant = restaurant;
+	}
+	
 }
--- src/main/java/services/ExpenseService.java
+++ src/main/java/services/ExpenseService.java
@@ -1,14 +1,70 @@
 package services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.webdevsummer12018.models.EssayQuestion;
+import com.example.webdevsummer12018.models.Exam;
+import com.example.webdevsummer12018.models.Question;
+
+import models.Expense;
+import repositories.AccomodationExpenseRepository;
 import repositories.ExpenseRepository;
+import repositories.FoodExpenseRepository;
+import repositories.OtherExpenseRepository;
+import repositories.TravelExpenseRepository;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class ExpenseService {
 	@Autowired 
+	GroupRepository groupRepo;
+	
+	@Autowired 
 	ExpenseRepository expenseRepo;
+	@Autowired 
+	TravelExpenseRepository travelExpRepo;
+	@Autowired 
+	FoodExpenseRepository foodExpRepo;
+	@Autowired 
+	AccomodationExpenseRepository accomodationExpRepo;
+	@Autowired 
+	OtherExpenseRepository otherExpRepo;
+	
+	@GetMapping("/api/group/{groupId}/expense")
+	public List<Expense> getAllExpenseByGroup(@PathVariable("groupId") int groupId) {
+		Optional<Group> optionalExam = groupRepo.findById(groupId);
+		if(optionalExam.isPresent()) {
+			Group group = optionalExam.get();
+			List<Expense> expenses = group.getExpenses();
+			return expenses;
+		}
+		else {
+			throw new IllegalArgumentException("Cannot find group " + groupId);
+		}
+	}
+	
+	@GetMapping("/api/{expenseType}/{eid}")
+	public Expense getExpenseById(
+			@PathVariable("expenseType") String expenseType,
+			@PathVariable("eid") int eid) {
+		try {
+			switch(expenseType) {
+			case "travel": return travelExpRepo.findById(eid).get(); break;
+			case "accomodation": return accomodationExpRepo.findById(eid).get(); break;
+			case "food": return foodExpRepo.findById(eid).get(); break;
+			case "other": return otherExpRepo.findById(eid).get(); break;
+			default: throw new IllegalArgumentException("No such expense of type " + expenseType);
+			}
+		} 
+		catch (Exception e) {
+			throw new IllegalArgumentException("Cannot find expense " + eid + " of type " + expenseType);
+		}
+	}
 }
